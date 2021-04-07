3f142e12a4ad0ad659745a43b78f0ee96368405f
Elias Binchamo <eliasbinchamo@gmail.com>
2021-04-07 17:26:20 -0400

    Updated

--- CSCI3236_Final_Project.java
+++ CSCI3236_Final_Project.java
@@ -1,5 +1,41 @@
-public class CSCI3236_Final_Project {
+import java.util.HashMap;
+import java.util.Map;
+
+
+    public class CSCI3236_Final_Project {private static char currentState = 'a';
+    private static Map<String,Character> map = new HashMap<String,Character>();
+
+
     public static void main(String[] args) {
-        // Test Program
+        
+        System.out.println("current state before the input: "+currentState);
+        //transition table
+        addToTheMap('a', 0, 'a');
+        addToTheMap('a', 1, 'b');
+        addToTheMap('b', 0, 'c');
+        addToTheMap('b', 1, 'b');
+        addToTheMap('c', 0, 'a');
+        addToTheMap('c', 1, 'c');
+        //starting state
+
+        currentState = 'a';
+
+        //test
+        System.out.println(map.toString());
+        currentState = DFA(1);
+
+        System.out.println("current state after the input: "+ currentState);
+
+    }
+
+    char getCurrentState(){
+        return currentState;
+    }
+
+    static void addToTheMap(char state, int input, char output){
+        map.put(""+state+input,output);
+    }
+    public static char DFA(int input){
+        return map.get(""+currentState+input);
     }
 }
