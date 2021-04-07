9973354c963fbd348ea98956bf498db9f68d347d
Elias Binchamo <eliasbinchamo@gmail.com>
2021-04-07 17:39:04 -0400

    changed

--- CSCI3236_Final_Project.java
+++ CSCI3236_Final_Project.java
@@ -1,43 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
+package CSCI3236_Final_Project;
 
-
-    public class CSCI3236_Final_Project {private static char currentState = 'a';
-    private static Map<String,Character> map = new HashMap<String,Character>();
-
+/**
+ * CSCI3236_Final_Project
+ */
+public class CSCI3236_Final_Project {
 
     public static void main(String[] args) {
         
-        System.out.println("current state before the input: "+currentState);
-        //transition table
-        addToTheMap('a', 0, 'a');
-        addToTheMap('a', 1, 'b');
-        addToTheMap('b', 0, 'c');
-        addToTheMap('b', 1, 'b');
-        addToTheMap('c', 0, 'a');
-        addToTheMap('c', 1, 'c');
-        //starting state
-
-        currentState = 'a';
-
-        //test
-        System.out.println(map.toString());
-        currentState = DFA(1);
-
-        System.out.println("current state after the input: "+ currentState);
-
-    }
-
-    char getCurrentState(){
-        return currentState;
-    }
-
-    //
-
-    static void addToTheMap(char state, int input, char output){
-        map.put(""+state+input,output);
-    }
-    public static char DFA(int input){
-        return map.get(""+currentState+input);
     }
 }
--- DFA.java
+++ DFA.java
@@ -1,3 +1,46 @@
-public class DFA {
-    // Class for the Deterministic Finite Automaton
+import java.util.HashMap;
+import java.util.Map;
+
+
+    public class DFA {
+        private static char currentState = 'a';
+        private static Map<String,Character> map = new HashMap<String,Character>();
+
+        public DFA(String filename){
+            
+        }
+    public static void main(String[] args) {
+        
+        System.out.println("current state before the input: "+currentState);
+        //transition table
+        addToTheMap('a', 0, 'a');
+        addToTheMap('a', 1, 'b');
+        addToTheMap('b', 0, 'c');
+        addToTheMap('b', 1, 'b');
+        addToTheMap('c', 0, 'a');
+        addToTheMap('c', 1, 'c');
+        //starting state
+
+        currentState = 'a';
+
+        //test
+        System.out.println(map.toString());
+        currentState = DFA(1);
+
+        System.out.println("current state after the input: "+ currentState);
+
+    }
+
+    char getCurrentState(){
+        return currentState;
+    }
+
+    //
+
+    static void addToTheMap(char state, int input, char output){
+        map.put(""+state+input,output);
+    }
+    public static char DFA(int input){
+        return map.get(""+currentState+input);
+    }
 }
