9fbbd44be4530e5f245f142b18989ab7d80197ee
Elias Binchamo <eliasbinchamo@gmail.com>
2021-04-07 18:55:02 -0400

    fddfd

--- CSCI3236_Final_Project.java
+++ CSCI3236_Final_Project.java
@@ -6,6 +6,11 @@ package CSCI3236_Final_Project;
 public class CSCI3236_Final_Project {
 
     public static void main(String[] args) {
+        DFA tst = new DFA("../test.txt");
+        tst.printMap();
+        System.out.println("Current State: "+tst.getCurrentState());
+        tst.ChangeState(0);
+        System.out.println("Current State: "+tst.getCurrentState());
         
     }
 }
--- DFA.java
+++ DFA.java
@@ -1,59 +1,47 @@
+package CSCI3236_Final_Project;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.util.Scanner;
+
 
     public class DFA {
-        private static char currentState = 'a';
-        private static Map<String,Character> map = new HashMap<String,Character>();
+        private  char currentState = 'a';
+        private  Map<String,Character> map = new HashMap<String,Character>();
 
         public DFA(String filename){
-            String alphabet;
-            String states;
-            String startState;
-            String acceptState;
-
-
-            String cleanedInput = cleanInput(alphabet);
+            try{
+            Scanner input = new Scanner(new File("test.txt"));
+            String alphabet = input.nextLine();
+            String states = input.nextLine();
+            String start = input.nextLine();
+            String accept = input.nextLine();
+            String tmp;
+            while (input.hasNext()) {
+                tmp = input.nextLine();
+                tmp = tmp.replaceAll("\\W+","");
+                System.out.println(tmp);
+                addToTheMap((char)tmp.charAt(0), tmp.charAt(1)-'0', tmp.charAt(2));
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        String cleanInput(String input){
-
         }
-    public static void main(String[] args) {
-        
-        System.out.println("current state before the input: "+currentState);
-        //transition table
-        addToTheMap('a', 0, 'a');
-        addToTheMap('a', 1, 'b');
-        addToTheMap('b', 0, 'c');
-        addToTheMap('b', 1, 'b');
-        addToTheMap('c', 0, 'a');
-        addToTheMap('c', 1, 'c');
-        //starting state
-
-        currentState = 'a';
-
-        //test
-        System.out.println(map.toString());
-        currentState = ChangeState(1);
-
-        System.out.println("current state after the input: "+ currentState);
-
-    }
-
     char getCurrentState(){
         return currentState;
     }
-
-    //
-
-    static void addToTheMap(char state, int input, char output){
+     void addToTheMap(char state, int input, char output){
         map.put(""+state+input,output);
     }
-    public static char ChangeState(int input){
-        int n =0;
-        return map.get(""+currentState+input);
+    public char ChangeState(int input){
+        char tmp = currentState;
+        currentState = map.get(""+currentState+input);
+        return map.get(""+tmp+input);
     }
-
+    public void printMap(){
+        System.out.println(map.toString());
+    }
     
 }
