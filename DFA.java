0d470a02123d7062fe7e0c0a3083b0ed8474b584
Elias Binchamo <eliasbinchamo@gmail.com>
2021-04-07 17:40:31 -0400

    fddfd

--- DFA.java
+++ DFA.java
@@ -7,7 +7,7 @@ import java.util.Map;
         private static Map<String,Character> map = new HashMap<String,Character>();
 
         public DFA(String filename){
-            
+                ///
         }
     public static void main(String[] args) {
         
