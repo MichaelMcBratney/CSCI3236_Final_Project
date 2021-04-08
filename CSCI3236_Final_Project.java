a40153c8d35510f1b92abfc6369c6ca6d5da1408
Anthony Pierce <Ant.pierce24@gmail.com>
2021-04-08 14:53:43 -0400

    Update CSCI3236_Final_Project.java
    Accepting text file in the command line

--- CSCI3236_Final_Project.java
+++ CSCI3236_Final_Project.java
@@ -1,19 +1,16 @@
-import java.io.File;
+import java.io.*;
+import java.util.*;
 
-/**
- * CSCI3236_Final_Project
- */
-public class CSCI3236_Final_Project {
+class Project{
 
-    public static void main(String[] args) {
-        DFA tst = new DFA("DFA-Test.txt");
-        for (int i = 0; i < args.length; i++){
-            System.out.println("String number " + (i+1) + " test: ");
-            tst.runDFA(args[i]);
-            System.out.println();
-            System.out.println();
-        }
+    public static void main(String[] args)throws FileNotFoundException 
+    {
+        DFA tst = new DFA(args[0]);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String x = input.nextLine();
+        tst.runDFA(x);
 
-        
+        input.close();
     }
 }
