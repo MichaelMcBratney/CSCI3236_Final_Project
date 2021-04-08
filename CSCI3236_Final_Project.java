c002c465b16c5dd8529532b4f53a91c6a41bc45e
mdmcb <mdmcbratney2000@gmail.com>
2021-04-08 11:55:03 -0400

    Added the ability to run several strings through the program

--- CSCI3236_Final_Project.java
+++ CSCI3236_Final_Project.java
@@ -7,7 +7,13 @@ public class CSCI3236_Final_Project {
 
     public static void main(String[] args) {
         DFA tst = new DFA("DFA-Test.txt");
-        tst.runDFA(args[0]);
+        for (int i = 0; i < args.length; i++){
+            System.out.println("String number " + (i+1) + " test: ");
+            tst.runDFA(args[i]);
+            System.out.println();
+            System.out.println();
+        }
+
         
     }
 }
