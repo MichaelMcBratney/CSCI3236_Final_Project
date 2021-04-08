2857eb8b5622da09cb7ac37889acc161935b91f4
mdmcb <mdmcbratney2000@gmail.com>
2021-04-08 15:08:25 -0400

    Re-added multiple strings functionality

--- CSCI3236_Final_Project.java
+++ CSCI3236_Final_Project.java
@@ -1,19 +1,22 @@
-import java.io.File;
+import java.io.*;
+import java.util.*;
 
-/**
- * CSCI3236_Final_Project
- */
-public class CSCI3236_Final_Project {
+class CSCI3236_Final_Project{
 
-    public static void main(String[] args) {
-        DFA tst = new DFA("DFA-Test.txt");
-        for (int i = 0; i < args.length; i++){
-            System.out.println("String number " + (i+1) + " test: ");
-            tst.runDFA(args[i]);
-            System.out.println();
-            System.out.println();
-        }
+    public static void main(String[] args)throws FileNotFoundException 
+    {
+        DFA tst = new DFA(args[0]);
+        Scanner input = new Scanner(System.in);
+        String x;
 
-        
+        System.out.print("Enter String: ");
+        x = input.nextLine();
+        do {
+            tst.runDFA(x);
+            System.out.print("\nEnter String (type exit when finished): ");
+            x = input.nextLine();
+        } while (x != "exit");
+
+        input.close();
     }
 }
